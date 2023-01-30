c748b683b0a4df1c4270c0abe455cba382cb8261
prycepavel <pryce.pavel@gmail.com>
2023-01-30 16:08:20 +0300

    Processing of the past date

--- food/js/script.js
+++ food/js/script.js
@@ -51,11 +51,20 @@ window.addEventListener('DOMContentLoaded', () => {
 
   // Функция определяющая разницу между текущим временем и дедлайном
   function getTimeRemaining(endtime) {
-    const t = Date.parse(endtime) - Date.parse(new Date()),
-      days = Math.floor(t / (1000 * 60 * 60 * 24)),
-      hours = Math.floor((t / (1000 * 60 * 60) % 24)),
-      minutes = Math.floor((t / 1000 / 60) % 60),
-      seconds = Math.floor((t / 1000) % 60);
+    let days, hours, minutes, seconds;
+    const t = Date.parse(endtime) - Date.parse(new Date());
+      
+    if (t <= 0) { // Проверяем, вышло ли время акции. Если вышло таймер выводит нули.
+      days = 0;
+      hours = 0;
+      minutes = 0;
+      seconds = 0;
+    } else {
+        days = Math.floor(t / (1000 * 60 * 60 * 24)),
+        hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+        minutes = Math.floor((t / 1000 / 60) % 60),
+        seconds = Math.floor((t / 1000) % 60);
+    }
     
     return {
       'total': t,
